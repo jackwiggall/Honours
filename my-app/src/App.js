05a7209a426d2a7c83d6550219ef1630dd2fd8f0
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2024-02-21 17:14:39 +0000

    home page in react

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Home from './home.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+{/*import 'bootstrap/dist/css/bootstrap.min.css';*/}
+
 
 function Header() {
 return (
@@ -11,13 +14,7 @@ return (
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
     <meta http-equiv='X-UA-Compatible' content='ie=edge' />
     <title>LoA</title>
-    <link
-  rel="stylesheet"
-  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-  integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-  crossorigin="anonymous"
-/>
-    <link rel='stylesheet' href='App.css' />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"/>
     <link rel='icon' href='images/icon.png' />
     <script src='https://kit.fontawesome.com/210a3569a8.js' crossorigin='anonymous'></script>
   </head>
