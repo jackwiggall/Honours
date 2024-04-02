5daeabf8a7182a0b0e4f915972c68f8be6c79be6
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2024-04-02 16:28:51 +0100

    query results success

--- src/publish.jsx
+++ src/publish.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Button } from "@aws-amplify/ui-react";
 import { useNavigate } from 'react-router-dom';
 
-
 import { generateClient } from 'aws-amplify/api';
-import { createGameInfo } from './graphql/mutations';
+import { createGameInfo, createPages } from './graphql/mutations';
+import { gameInfosByAccountsID } from './graphql/queries';
 
 const client = generateClient();
 
 function PublishExtra() {
 
-  if (localStorage.getItem("id")!==null && localStorage.getItem("storyDetails")!==null && localStorage.getItem("storyNum")!==null) {
+  if (localStorage.getItem("id")!==null&&localStorage.getItem("storyDetails")!==null&&localStorage.getItem("storyNum")!==null&&localStorage.getItem("pageDetails")!==null) {
     const accountsID = localStorage.getItem("id");
 
     var newDetails = JSON.parse(localStorage.getItem("storyDetails"));
+    var pageDetails = JSON.parse(localStorage.getItem("pageDetails"));
     var storyNum = Number(localStorage.getItem("storyNum"));
 
     //set form data
@@ -21,10 +22,9 @@ function PublishExtra() {
     const shortDesc = newDetails[storyNum].shortDesc;
     const longDesc = newDetails[storyNum].longDesc;
     const genre = newDetails[storyNum].genre;
-    console.log(`${accountsID}, ${title}, ${shortDesc}, ${longDesc}, ${genre}`);
+    //console.log(`${accountsID}, ${title}, ${shortDesc}, ${longDesc}, ${genre}`);
 
-
-    const results = client.graphql({
+    /*const results = client.graphql({
       query: createGameInfo,
       variables: {
         input: {
@@ -34,10 +34,46 @@ function PublishExtra() {
           longDesc : longDesc,
         },
       },
+    });*/
+    const results = client.graphql({
+      query: gameInfosByAccountsID, variables: {accountsID : accountsID}
+    })
+    .then(function(v) { // `delay` returns a promise
+      console.log(v); // Log the value once it is resolved
+      v.data.gameInfosByAccountsID.items.forEach(x => {
+        console.log(x);
+        //check if current game is same as just created for id
+      })  
+    })
+    .catch(function(v) {
+      // Or do something else if it is rejected
+      // (it would not happen in this example, since `reject` is not called).
+      console.log("error");
     });
+      /*results.data.
+
+      //if game is published will need to get gameID
+    /*pageDetails.forEach(x => {
+      console.log(x);
+      /*const results = client.graphql({
+        query: createPages,
+        variables: {
+          input: {
+            gameID :,
+            localID : x.id,
+            title : x.title,
+            text : x.text,
+            linkText : x.linkText,
+            linkID : x.linkID,
+          },
+        },
+      });
+    })*/
+
+
   }else {
 
-    //console.log("error");
+    console.log("error");
   }
 
 }
@@ -52,9 +88,9 @@ function Publish() {
       //ATTEMPT PUBLISH PROJECT
       PublishExtra();
       //removes all pages and stories (need to change to remove single game)
-      localStorage.removeItem("storyDetails");
-      localStorage.removeItem("pageDetails");
-      localStorage.removeItem("tagList");
+      //localStorage.removeItem("storyDetails");
+      //localStorage.removeItem("pageDetails");
+      //localStorage.removeItem("tagList");
       nav('../library'); // Redirect to library
     }
 
--- src/results.jsx
+++ src/results.jsx
@@ -1,42 +1,67 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { generateClient } from 'aws-amplify/api';
 
 import { listGameInfos } from './graphql/queries';
 
+
 function LoopedResult() {
   //NEED TO ADD KEY
   //SHOWS ALL PAGES
-  const client = generateClient();
-  const results = client.graphql({ query: listGameInfos, limit: 1}); //change limit once working
-  console.log(results);
-  const details = results.data;
-  console.log(`boxes ${details}`); //returning undefined?
-  try {
-    return (
-      <div>
-      {details.data.listGameInfos.map((item) => (
-        <div className='box'>
-          <div className='card' style={{width: '100%', marginBottom: '20px'}}>
-            <Link to={"./info"} style={{color: 'inherit', textDecoration: 'none'}}>
-            <div className='card-body'>
-              <h5 className='card-title'>{item.title}</h5>
-              <p className='card-text d-inline-block'>{item.shortDesc}</p>
-            </div>
-            <ul className='list-group list-group-flush text-secondary'>
-              <li className='list-group-item'>#fantasy, #sci-fi</li>
-            </ul>
-            </Link>
-          </div>
-        </div>
+  var run = useRef(false);
+  var items = [{}];
+  const [res, setRes] = useState([{}]);
 
-      ))}
+  if (!run.current) {
+    //still runs twice, doesnt prevent single run
+    run.current = true;
 
-      </div>
-    )}
-  catch {
-    <h2 className='centre' style={{marginTop: "5px"}}>No Results!</h2>
+    const client = generateClient();
+    const results = client.graphql({ query: listGameInfos, limit: 1}) //change limit later, just to save network/query space
+    .then(function(v) { // `delay` returns a promise
+      //console.log(v.data.listGameInfos.items); // Log the value once it is resolved
+
+        setRes(v.data.listGameInfos.items);
+
+    }) //change limit once working
+    .catch(function(v) {
+      // Or do something else if it is rejected
+      // (it would not happen in this example, since `reject` is not called).
+      console.log("error");
+    });
   }
+  if (res.length!==0) {
+    for (var i=0; i < res.length; i++) {
+      return (
+        <DisplayResults data={res[i]}/>
+      )
+   }
+  }else {
+    return (
+      <h2 className='centre' style={{marginTop: "5px"}}>No Results!</h2>
+    )
+  }
+}
+
+function DisplayResults(i) {
+  //console.log(i.data); //takes a second to load
+    return (
+      <>
+    <div className='box' onClick={() => localStorage.setItem("storyNum",0)}> {/*set to id*/}
+      <div className='card' style={{width: '100%', marginBottom: '20px'}}>
+        <Link to={'./info'} style={{color: 'inherit', textDecoration: 'none'}}>
+        <div className='card-body'>
+          <h5 className='card-title'>{i.data.title}</h5>
+          <p className='card-text d-inline-block'>{i.data.shortDesc}</p>
+        </div>
+        <ul className='list-group list-group-flush text-secondary'>
+          <li className='list-group-item'>#fantasy, #sci-fi</li>
+        </ul>
+        </Link>
+      </div>
+    </div>
+  </>
+  )
 }
 
 function Results() {
@@ -49,8 +74,7 @@ function Results() {
       run.current = true;
       return (
         <div>
-          {/*<LoopedResult /> {/*Still runs twice even tho rendered once with ref*/}
-          {/*Disabled query rn as not working*/}
+          <LoopedResult />
         </div>
       )
     }
