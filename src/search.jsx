7a85d1441ed38033cf9d5ab80f0948196e4a7a61
Jack Wiggall <72393388+jackwiggall@users.noreply.github.com>
2024-04-18 16:49:01 +0100

    search reload fix

--- src/search.jsx
+++ src/search.jsx
@@ -12,8 +12,8 @@ function Search() {
 
       e.preventDefault();
       localStorage.removeItem("searchRes"); //next time viewing page will requery db, need to add reload
-      //console.log("resetting");
-      window.location.reload(); //reloads page so can requery db
+      document.location.reload();
+
     }
 
 if (!run.current) {
